b4bbf1b9c986056260550f46db095c9ed834cb20
Eugene Nesvetaev <dev@nesvet.dev>
2026-02-17 00:11:01 +0400

    fix(core): Export DateError as value

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type { DateError } from "./BaseTimeline";
+export { DateError } from "./BaseTimeline";
 export * from "./RangeTimeline";
 export * from "./Timeline";
 export type { Day, Midnight, Range, RawDate } from "./types";
